b24ebcc3fd48674fe6318b8cad159b5d0a6ac192
Serkan ACAR <serkan.acr6778@gmail.com>
2022-09-27 16:29:42 +0300

    # club_vs error views

--- sporbacks/home/admin.py
+++ sporbacks/home/admin.py
@@ -4,4 +4,7 @@ from .models import *
 # Register your models here.
 
 admin.site.register(sosyal_media)
-admin.site.register(clubs)
+class club_data(admin.ModelAdmin):
+    list_display = ("takim_isim","takim_isim_kisaltma","takim_logo")
+admin.site.register(clubs, club_data)
+admin.site.register(club_point)
